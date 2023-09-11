7029c7d81a43de6402f2d7f9368d41e51ea7c83e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-12 00:33:11 +0530

    Update weatherController.js

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,28 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 
 async function getDataFromDatabase() {
-  return new Promise((resolve, reject) => {
-    fs.readFile('src/data/data.json', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(JSON.parse(data));
-      }
-    });
-  });
+	return new Promise((resolve, reject) => {
+		fs.readFile("./data.json", (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(JSON.parse(data));
+			}
+		});
+	});
 }
 
 async function saveDataToDatabase(data) {
-  return new Promise((resolve, reject) => {
-    const jsonData = JSON.stringify(data);
-    fs.writeFile('src/data/data.json', jsonData, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+	return new Promise((resolve, reject) => {
+		const jsonData = JSON.stringify(data);
+		fs.writeFile("./data.json", jsonData, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
+// Level 1: Get City Weather Data by Name
+
+const data = JSON.parse(fs.readFileSync(`${__dirname}../controllers/data.json`));
+
+async function getWeatherDataByName(cityName) {
+	let name = cityName.toLowerCase();
+	const fetchedData = data.find((item) => item.city.toLowerCase() === name);
+	// console.log(fetchedData);
+	return fetchedData;
 }
 
 /*
@@ -37,16 +48,25 @@ async function saveDataToDatabase(data) {
 
 */
 
-
 // Level 2: Get 7 Days Weather Forecast Data by Name
 async function getForecastDataByName(cityName) {
-  
-
-  // TODO: Implement this function
-  
+	// TODO: Implement this function
+	// console.log(cityName);
+	let name = cityName.toLowerCase();
+	// let = forecast;
+	const fetchedData = data.find((item) => {
+		if (item.city.toLowerCase() === name) {
+			let forecast = item.forecast;
+			return forecast;
+		} else {
+			return false;
+		}
+	});
+	// console.log(forecast);
+	return fetchedData.forecast;
 }
 
-
 module.exports = {
-  getForecastDataByName
+	getWeatherDataByName,
+	getForecastDataByName,
 };
